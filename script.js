12df2b56dfd8899736b76749466f64087dec31ec
Nico11119 <n.kienesberger@htlkrems.at>
2026-03-09 12:52:46 +0100

    auskommentieren

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 function onSubmitClick(event) {
   event.preventDefault();
 
+  // daten auslesen
   const formElement = document.getElementById("personalData");
   const formularData = new FormData(formElement);
 
+  //neues objekt hinzufügen wo die daten reingespiechert werden
   const neuePerson = {};
   for (const entry of formularData.entries()) {
     console.log("Name des Feldes: ", entry[0]);
@@ -13,6 +15,7 @@ function onSubmitClick(event) {
     neuePerson[entry[0]] = entry[1];
   }
 
+  //den eintrag im localstorage speichern
   let personenListe = JSON.parse(localStorage.getItem("personData")) || [];
   personenListe.push(neuePerson);
   localStorage.setItem("personData", JSON.stringify(personenListe));
@@ -25,11 +28,13 @@ function onSubmitClick(event) {
 
   console.log("submit button clicked");
 
+  //die daten anzeigen, wenn die funktion da ist
   if (typeof formsAnzeigen === "function") {
     formsAnzeigen();
   }
 }
 
+//submit event hinzufügen, dass man mit enter submitten kann
 const formElement = document.getElementById("personalData");
 if (formElement) {
   formElement.addEventListener("submit", onSubmitClick);
@@ -37,14 +42,18 @@ if (formElement) {
 
 // ======= Übersicht =========
 
+
 const formDataDiv = document.getElementById("formularData");
 
+// formulardaten werden angzeigt
 function formsAnzeigen() {
   if (!formDataDiv) return;
 
+  //alles aus dem localstorage holen
   const alleFormulare = JSON.parse(localStorage.getItem("personData"));
   formDataDiv.innerHTML = "";
 
+  //wenn keine daten da sind, wird die angezeigt, dass nix da ist, sonst wird es angezeigt
   if (!alleFormulare || alleFormulare.length === 0) {
     formDataDiv.innerHTML = "<p>Keine Formulare gefunden :(</p>";
   } else {
@@ -54,6 +63,7 @@ function formsAnzeigen() {
       for (const key in Person) {
         html += "<p><strong>" + key + ":</strong> " + Person[key] + "</p>";
       }
+      // fügt button hinzu zum eintrag löschen
       html += `<button onclick="einzelnenEintragLöschen(${i})">Eintrag löschen</button>`;
       html += "</div><hr>";
     });
@@ -61,6 +71,7 @@ function formsAnzeigen() {
   }
 }
 
+//funktion zum einzenen eintrag löschen, die den index vom löschenden eintrag bekommt und den löscht und dann das anzeigen aktuallisirt
 function einzelnenEintragLöschen(index) {
   let listeDerPersonen = JSON.parse(localStorage.getItem("personData")) || [];
   listeDerPersonen.splice(index, 1);
@@ -68,6 +79,7 @@ function einzelnenEintragLöschen(index) {
   formsAnzeigen();
 }
 
+// funktion zum löschen der ganzen einträge; löcalstorage wird gelöscht und neu angezeigt
 function eintragLöschen() {
   if (confirm("Möchtest du wirklich alle Formulare löschen?")) {
     localStorage.removeItem("personData");
@@ -75,4 +87,5 @@ function eintragLöschen() {
   }
 }
 
+//anzeigen
 formsAnzeigen();
